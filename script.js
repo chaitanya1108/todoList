e9a30d62491e57a32bccc94741b00bfef01b9e51
Chaitanya Kumar <chaitanya4real@gmail.com>
2021-06-23 21:04:07 +0530

    enter button functionality

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let inpNewTask=$('#inpNewTask')
 let btnadd=$('#btnadd')
 let btnclear=$('#btnclear')
 
-btnadd.click(()=>{
+function additem(){
     let listitem= $('<li>',{ //to create an element u dont need a closing tag
         'class':'list-group-item', //class is a reserved word in js so use quotation
         text: inpNewTask.val()
@@ -13,5 +13,15 @@ btnadd.click(()=>{
     })
     ulTasks.append(listitem)
     inpNewTask.val("") //also have to replace it with an empty string after its clicked once
+}
+
+inpNewTask.keypress((e)=>{
+    if(e.which == 13){ //13 is the keycode for the enter key, can check other key codes by 'console.log(e.which)'
+        additem()
+    }
+})
+
+btnadd.click(()=>{
+    additem()
 })
 btnclear.click(()=>inpNewTask.val(""))
